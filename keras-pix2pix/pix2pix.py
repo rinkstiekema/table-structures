99851f19a8b80bb270e7c8bbcd267fc526e4d48c
rinkstiekema <rinkstiekema@hotmail.com>
2019-04-20 17:37:03 +0200

    Removed needless imports

--- keras-pix2pix/pix2pix.py
+++ keras-pix2pix/pix2pix.py
@@ -1,13 +1,10 @@
 from __future__ import print_function, division
-import scipy
 
-from keras.datasets import mnist
-from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers import Input, Dropout, Concatenate
+from keras.layers import BatchNormalization, Activation
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.optimizers import Adam
 import datetime
 import matplotlib.pyplot as plt
