b4153a238853b245854fcc7072c008c3b66b1cfc
rinkstiekema <rinkstiekema@hotmail.com>
2019-05-12 18:31:07 +0200

    Added predict options and predict python script

---
+++ pipeline/pix2pixHD/options/predict_options.py
@@ -0,0 +1,14 @@
+### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
+### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+from .base_options import BaseOptions
+
+class PredictOptions(BaseOptions):
+    def initialize(self):
+        BaseOptions.initialize(self)
+        self.parser.add_argument('--results_dir', type=str, default='./outlines/', help='saves results here.')
+        self.parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
+        self.parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
+        self.parser.add_argument('--how_many', type=int, default=float("inf"), help='how many images to predict')       
+        self.parser.add_argument('--cluster_path', type=str, default='features_clustered_010.npy', help='the path for clustered results of encoded features')
+        self.isTrain = False
+        self.isPredict = True
---
+++ pipeline/pix2pixHD/predict.py
@@ -0,0 +1,58 @@
+### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
+### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+import os
+from collections import OrderedDict
+from torch.autograd import Variable
+from options.predict_options import PredictOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+import util.util as util
+from util.visualizer import Visualizer
+from util import html
+import torch
+
+opt = PredictOptions().parse(save=False)
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+visualizer = Visualizer(opt)
+# test
+model = create_model(opt)
+if opt.data_type == 16:
+    model.half()
+elif opt.data_type == 8:
+    model.type(torch.uint8)
+        
+if opt.verbose:
+    print(model)
+else:
+    from run_engine import run_trt_engine, run_onnx
+    
+for i, data in enumerate(dataset):
+    if i >= opt.how_many:
+        break
+    if opt.data_type == 16:
+        data['label'] = data['label'].half()
+        data['inst']  = data['inst'].half()
+    elif opt.data_type == 8:
+        data['label'] = data['label'].uint8()
+        data['inst']  = data['inst'].uint8()
+
+    minibatch = 1 
+    if opt.engine:
+        generated = run_trt_engine(opt.engine, minibatch, [data['label'], data['inst']])
+    elif opt.onnx:
+        generated = run_onnx(opt.onnx, opt.data_type, minibatch, [data['label'], data['inst']])
+    else:        
+        generated = model.inference(data['label'], data['inst'], data['image'])
+        
+    visual = ('synthesized_image', util.tensor2im(generated.data[0]))
+    img_path = data['path']
+    print('process image... %s' % img_path)
+    visualizer.save_images_predict(visual, img_path, opt.results_dir)
+
+webpage.save()
