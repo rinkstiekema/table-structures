7620a8032ff9d12d6f385f805baa5a23138257a3
rinkstiekema <rinkstiekema@hotmail.com>
2019-05-12 21:48:11 +0200

    Specify DPI for image generation

--- pipeline/pdffigures2/src/main/scala/org/allenai/pdffigures2/FigureExtractorBatchCli.scala
+++ pipeline/pdffigures2/src/main/scala/org/allenai/pdffigures2/FigureExtractorBatchCli.scala
@@ -29,7 +29,7 @@ object FigureExtractorBatchCli extends Logging {
 
   case class CliConfigBatch(
                              inputFiles: Seq[File] = Seq(),
-                             figureDataPrefix: Option[String] = None, dpi: Int = 150, ignoreErrors: Boolean = false,
+                             figureDataPrefix: Option[String] = None, dpi: Int = 72, ignoreErrors: Boolean = false,
                              saveStats: Option[String] = None, saveRegionlessCaptions: Boolean = false,
                              threads: Int = 1, debugLogging: Boolean = true, fullTextPrefix: Option[String] = None,
                              figureImagePrefix: Option[String] = None, figureFormat: String = "png", figType: String = "All"
