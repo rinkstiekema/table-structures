10149127a3281220adf815f3236f4d77ca908422
rinkstiekema <rinkstiekema@hotmail.com>
2019-05-15 15:08:27 +0200

    Work on 1024x1024

--- pipeline/rulers.py
+++ pipeline/rulers.py
@@ -94,9 +94,7 @@ def rule(json_folder):
 			tables = json.load(jfile) # current json file
 			for table in tables:
 				try:
-					original_size = (table["regionBoundary"]["x2"] - table["regionBoundary"]["x1"], table["regionBoundary"]["y2"] - table["regionBoundary"]["y1"])
-					original_size = (int(original_size[0]*72/25.4), int(original_size[1]*72/25.4))
-					img = cv2.imread(table["outlineURL"])[0:int(original_size[1]), 0:int(original_size[0])]
+					img = cv2.imread(table["outlineURL"])
 					gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 					img = cv2.Canny(gray, 500, 500,apertureSize = 3)
 					kernel = np.ones((3,3),np.uint8)
