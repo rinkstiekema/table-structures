62f6ed611e0b946f1020db589fe24a37f7b5e6c8
rinkstiekema <rinkstiekema@hotmail.com>
2019-05-17 16:29:13 +0200

    Try except removed

--- pipeline/split-segment.py
+++ pipeline/split-segment.py
@@ -9,15 +9,18 @@ original_length = len(images)
 
 types = [{"name":"train", "range": 50}, {"name":"val", "range": 25}, {"name":"test", "range": 25}]
 for t in types:
-    try:
-        os.mkdir(os.path.join(location, t["name"]))
-    try:
-        os.mkdir(os.path.join(location, t["name"]+"_label"))
+    img_location = os.path.join(location, t["name"])
+    if not os.path.exists(img_location):
+        os.mkdir(img_location)
+    
+    label_location = os.path.join(location, t["name"]+"_label")
+    if not os.path.exists(label_location):  
+        os.mkdir(label_location)
 
     data = images[:original_length/100 * t["range"])]
 
     for image in data:
-        os.rename(os.path.join(location, "train_A", image), os.path.join(location, t["name"], image))
-        os.rename(os.path.join(location, "train_B", image), os.path.join(location, t["name"]+"_label", image))
+        os.rename(os.path.join(location, "train_A", image), os.path.join(img_location, image))
+        os.rename(os.path.join(location, "train_B", image), os.path.join(label_location, image))
 
     images = images[original_length/100 * t["range"])+1:]
