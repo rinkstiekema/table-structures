a373019e432d8b947d0e728a6ad07229c80a7733
rinkstiekema <rinkstiekema@hotmail.com>
2019-06-14 16:48:01 +0200

    Try and catch finding cells property in json

--- pipeline/json2csv.py
+++ pipeline/json2csv.py
@@ -6,27 +6,30 @@ from tqdm import tqdm
 
 def json2csv(json_folder, csv_folder):
     for json_file in os.listdir(json_folder):
-        json_location = os.path.join(json_folder, json_file)
-        with open(json_location, 'r+') as jfile:
-            tables = json.load(jfile)
-            
-            for table in tables:
-                # Unique occurences of rows and columns
-                rows = list(set([x["rect"][0][1] for x in table["cells"]]))
-                columns = list(set([x["rect"][0][0] for x in table["cells"]]))
-                # Replace row/col by their index
-                index_rows = sorted(list(set([cell["rect"][0][1] for cell in table["cells"]])))
-                index_columns = sorted(list(set([cell["rect"][0][0] for cell in table["cells"]])))
+        try:
+            json_location = os.path.join(json_folder, json_file)
+            with open(json_location, 'r+') as jfile:
+                tables = json.load(jfile)
+                
+                for table in tables:
+                    # Unique occurences of rows and columns
+                    rows = list(set([x["rect"][0][1] for x in table["cells"]]))
+                    columns = list(set([x["rect"][0][0] for x in table["cells"]]))
+                    # Replace row/col by their index
+                    index_rows = sorted(list(set([cell["rect"][0][1] for cell in table["cells"]])))
+                    index_columns = sorted(list(set([cell["rect"][0][0] for cell in table["cells"]])))
 
-                matrix = [['' for _ in range(len(columns))] for _ in range(len(rows))]
-                for cell in table["cells"]:
-                    x = index_rows.index(cell["rect"][0][1])
-                    y = index_columns.index(cell["rect"][0][0])
-                    matrix[x][y] = cell["words"]
-                    
-                df = pd.DataFrame(matrix)
-                csv_name = os.path.splitext(table["name"])[0] + ".csv"
-                df.to_csv(os.path.join(csv_folder, csv_name), index=False, header=False)
+                    matrix = [['' for _ in range(len(columns))] for _ in range(len(rows))]
+                    for cell in table["cells"]:
+                        x = index_rows.index(cell["rect"][0][1])
+                        y = index_columns.index(cell["rect"][0][0])
+                        matrix[x][y] = cell["words"]
+                        
+                    df = pd.DataFrame(matrix)
+                    csv_name = os.path.splitext(table["name"])[0] + ".csv"
+                    df.to_csv(os.path.join(csv_folder, csv_name), index=False, header=False)
+        except:
+            continue
 
 
 
