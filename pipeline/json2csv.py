ff5d7ce631e853f19eaa96a8d0894e9536e02248
rinkstiekema <rinkstiekema@hotmail.com>
2019-07-06 18:27:11 +0200

    Json location correction

--- pipeline/json2csv.py
+++ pipeline/json2csv.py
@@ -9,7 +9,7 @@ def json2csv(json_folder, csv_folder):
     for json_file in os.listdir(json_folder):
         try:
             json_location = os.path.join(json_folder, json_file)
-            with open(json_location, 'r+', encoding=utils.get_encoding_type(json_file_location), errors='ignore') as jfile:
+            with open(json_location, 'r+', encoding=utils.get_encoding_type(json_location), errors='ignore') as jfile:
                 tables = json.load(jfile)
                 
                 for table in tables:
