1124cb56de5791faeda662297fc356ca98230e76
rinkstiekema <rinkstiekema@hotmail.com>
2019-07-07 18:34:00 +0200

    .

--- pipeline/textboxtract.py
+++ pipeline/textboxtract.py
@@ -15,7 +15,7 @@ def texboxtract_synthetic(pdf, table):
     words = page.getTextWords()
     for idx, cell in enumerate(table["cells"]):
         rect = [cell[0][0]+table["regionBoundary"]["x1"], cell[0][1]+table["regionBoundary"]["y1"], cell[1][0]+table["regionBoundary"]["x1"], cell[1][1]+table["regionBoundary"]["y1"]]
-        rect = list(map(lambda i: i*72/table["dpi"], rect))
+        rect = list(map(lambda i: i*0.72, rect))
 
         mywords = [w for w in words if fitz.Rect([(w[0]+w[2])/2,(w[1]+w[3])/2,(w[0]+w[2])/2+1,(w[1]+w[3])/2+1]) in fitz.Rect(rect)]
         mywords.sort(key = itemgetter(3, 0))   # sort by y1, x0 of the word rect
