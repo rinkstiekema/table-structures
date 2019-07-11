2ab05508498a0f1203c28ae1b56cd71233fd4e2d
rinkstiekema <rinkstiekema@hotmail.com>
2019-07-11 22:13:27 +0200

    Padding test function

--- pipeline/test.py
+++ pipeline/test.py
@@ -2,16 +2,21 @@ import sys
 import os
 import cv2 
 import rulers 
+import pad
 
-png = cv2.imread(sys.argv[1])
-cells, intersections = rulers.rule_test(png)
-print(intersections)
-for cell in cells:
-    cv2.rectangle(png, cell[0], cell[1], (0,0,0),1)
+directory = sys.argv[1]
+for i in os.listdir(directory):
+    pad.pad_image(os.path.join(directory, i), (1024, 1024))
 
+# png = cv2.imread(sys.argv[1])
+# cells, intersections, img = rulers.rule_test(png)
+# print(intersections)
+# # for cell in cells:
+# #     cv2.rectangle(png, cell[0], cell[1], (),1)
+# img = cv2.cvtColor(img,cv2.COLOR_GRAY2B)
 # for intersection in intersections:
-#     cv2.circle(png, intersection, 1, (0,0,0),1)
+#     cv2.circle(img, intersection, 1, (255,0,0), 3)
 
-cv2.imshow('image', png)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
